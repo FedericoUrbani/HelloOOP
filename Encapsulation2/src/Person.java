ff6d68ec7b3f20b44a2456806920cfd03e55e4ff
Federico Urbani <urbanifederico5@gmail.com>
2023-01-20 15:55:41 +0100

    Updated Encapsulation2

---
+++ Encapsulation2/src/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+
+    String name;
+    String surname;
+    double height;
+    int age;
+
+    public Person(String name, String surname, double height, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.height = height;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", height=" + height +
+                ", age=" + age +
+                '}';
+    }
+}
